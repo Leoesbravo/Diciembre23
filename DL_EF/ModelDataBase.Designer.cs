47ac8875a9294ac92d5a1f903d289648ec281915
Joserbp <josera.jack@gmail.com>
2024-01-08 16:38:52 -0600

    4.8

--- DL_EF/ModelDataBase.Designer.cs
+++ DL_EF/ModelDataBase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\digis\Documents\IISExpress\Leonardo Escogido Bravo\LEscogidoProgramacionNCapas\DL_EF\ModelDataBase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\digis\source\Dic\DL_EF\ModelDataBase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
