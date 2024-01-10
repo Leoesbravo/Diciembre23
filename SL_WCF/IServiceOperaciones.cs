6d346730e7ac8ae2001dec6d4d69f8c5e404623a
Joserbp <josera.jack@gmail.com>
2024-01-09 18:19:47 -0600

    WCF

---
+++ SL_WCF/IServiceOperaciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SL_WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceOperaciones" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceOperaciones
+    {
+        //Interfaz -- Define el comportamiento de las clases que hereden de ella
+        //Plantilla metodos sin implementación
+        //Herencia -- Obliga a implementar los metodos declarados
+        [OperationContract]
+        void DoWork();  //Firma de metodos
+
+        [OperationContract]
+        string Saludar(string nombre);
+
+        //Como mandar a llamar los metodos de BL Producto/Transporte
+
+    }
+}
